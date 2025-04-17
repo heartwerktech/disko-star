e7b09e0d64878148cff564cf28a2952d2c64ecc6
David Speith <david@heartwerk.tech>
2025-04-17 08:40:10 +0200

    minor

--- src/config.h
+++ src/config.h
@@ -11,9 +11,9 @@
 #define ENABLE_SERIAL 1
 
 #define ENABLE_SERVER 1
-#define ENABLE_MQTT 0
+#define ENABLE_MQTT 1
 
-#define ENABLE_MOTOR 0
+#define ENABLE_MOTOR 1
 #define ENABLE_STEPPER 1
 
 #define ENABLE_LEDS 0
